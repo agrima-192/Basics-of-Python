07522779cce99f6b5da36a57ab1c060637f409d5
Agrima Khandelwal <agrima19feb@gmail.com>
2026-04-20 23:11:08 +0530

    Add program to swap two numbers
    Program demonstrates swapping of two numbers without using an additional variable.

---
+++ swap_numbers.py
@@ -0,0 +1,12 @@
+#Program to swap two numbers without taking additional variable.
+
+a=6
+b=9
+
+print("Value of a before swapping:",a)
+print("Value of b before swapping:",b)
+
+a,b=b,a
+
+print("Value of a after swapping:",a)
+print("Value of b after swapping:",b)
