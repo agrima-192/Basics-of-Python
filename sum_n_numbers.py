07e38ecdd8f18fce5b8ac851ae8159d4aa830ab0
Agrima Khandelwal <agrima19feb@gmail.com>
2026-04-21 22:18:26 +0530

    Add program to calculate sum of first n natural numbers

---
+++ sum_n_numbers.py
@@ -0,0 +1,5 @@
+#Program to find sum of first n natural numbers.
+
+n=int(input("Enter the natural number upto which the sum is to be calculated:\n"))
+nsum = (n**2 + n)//2
+print("The sum upto first",n,"natural numbers is :",nsum)
