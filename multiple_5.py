dbf9e99ccda7925d11c82489e3e689b43cbf4146
Agrima Khandelwal <agrima19feb@gmail.com>
2026-04-26 18:16:12 +0530

    Implement multiple of five checker
    Added a program to check if a number is a multiple of five.

---
+++ multiple_5.py
@@ -0,0 +1,8 @@
+#Program to check whether the given number is multiple of five or not.
+
+x=int(input("Enter a number to check : "))
+
+if (x%5==0) :
+    print ("The number",x,"is a multiple of five.")
+else :
+    print ("The number",x,"is NOT a mupltiple of five.")
